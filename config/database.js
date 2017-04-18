832baa67d332ebf0ad591f42882c040cc1330664
dessHub <deskip95@gmail.com>
2017-04-18 22:10:32 +0300

    m

--- config/database.js
+++ config/database.js
@@ -1,13 +1,13 @@
 
 module.exports=[
 	{
-    'environment': 'development',
+    'environment': 'production',
     'url'       : 'mongodb://desshub:kejahunt@ds163010.mlab.com:63010/kejahunt',
     'secret'    : 'nosecrets'
   },
 
   {
-    'environment': 'production',
+    'environment': 'productio',
     'url'       : process.env.MONGOLAB_URI,
     'secret'    : 'nosecrets'
   },
